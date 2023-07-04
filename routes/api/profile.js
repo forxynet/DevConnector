c73f6b206ca304778b2e218c66f06733ad299c91
Hakan Baştürk <forxynet@gmail.com>
2023-07-04 17:18:45 +0300

    Remove users posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,8 @@ const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
+const Post = require('../../models/Post');
+
 const { response } = require("express");
 
 // @route    GET api/profile/me
@@ -151,8 +153,8 @@ router.get("/user/:user_id", async (req, res) => {
 // @access   private
 router.delete("/", auth, async (req, res) => {
   try {
-    // @todo - remove users posts
-
+    // Remove users posts
+    await Post.deleteMany({user: req.user.id});
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
     // Remove user
