8b0a9214ca4c6f591a99e9bb9232e8f0366412c1
Hakan Baştürk <forxynet@gmail.com>
2023-07-04 14:50:21 +0300

    updated dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, { Fragment,useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import ProfileForm from './components/profile-forms/ProfileForm';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
 import { Provider } from "react-redux";
@@ -31,6 +36,26 @@ const App = () => {
           <Route path="/" element={<Landing />} />
           <Route path="register" element={<Register />} />
           <Route path="login" element={<Login />} />
+          <Route
+            path="dashboard"
+            element={<PrivateRoute component={Dashboard} />}
+          />
+          <Route
+            path="create-profile"
+            element={<PrivateRoute component={ProfileForm} />}
+          />
+          <Route
+            path="edit-profile"
+            element={<PrivateRoute component={ProfileForm} />}
+          />
+          <Route
+            path="add-experience"
+            element={<PrivateRoute component={AddExperience} />}
+          />
+          <Route
+            path="add-education"
+            element={<PrivateRoute component={AddEducation} />}
+          />
         </Routes>
       </Router>
     </Provider>
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -10,27 +10,22 @@ const User = require("../../models/User");
 const { response } = require("express");
 
 // @route    GET api/profile/me
-// @desc     Get logged in profile
+// @desc     Get current users profile
 // @access   Private
-router.get("/me", auth, async (req, res) => {
+router.get('/me', auth, async (req, res) => {
   try {
-    const profile = await Profile.findOne({ user: req.user.id }).populate(
-      "user",
-      ["name", "avatar"]
-    );
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate('user', ['name', 'avatar']);
 
     if (!profile) {
-      return res.status(400).json({
-        message: "There is no profile for this user",
-      });
+      return res.status(400).json({ msg: 'There is no profile for this user' });
     }
 
     res.json(profile);
-  } catch (error) {
-    console.log(error.message);
-    res.status(500).json({
-      message: error.message,
-    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
   }
 });
 
